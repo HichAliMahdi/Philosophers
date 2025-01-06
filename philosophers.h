9bc7a38cb23cf5ebf529d449319f4681404966f4
hali-mah <hichalimahdi@gmail.com>
2025-01-06 12:26:23 +0100

    first commit, init of the project header and files

---
+++ philosophers.h
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/06 11:52:32 by hali-mah          #+#    #+#             */
+/*   Updated: 2025/01/06 12:25:35 by hali-mah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <limits.h>
+
+typedef pthread_mutex_t	t_mtx;
+
+typedef struct s_fork
+{
+	t_mtx	fork;
+	int		fork_id;
+}	t_fork;
+
+typedef struct s_philo
+{
+	int			id;
+	long		meals_counter;
+	bool		full;
+	long		las_meal_time;
+	t_fork		*left_fork;
+	t_fork		*right_fork;
+	pthread_t	thread_id;
+}	t_philo;
+
+typedef struct s_table
+{
+	long	philo_nbr;
+	long	time_to_die;
+	long	time_to_eat;
+	long	time_to_sleep;
+	long	nbr_limit_meals;
+	long	start_simulation;
+	long	end_simulation;
+	t_fork	*forks;
+	t_philo	*philos;
+}	t_table;
+
+#endif
